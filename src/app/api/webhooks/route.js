7a2484a0b389dd74f2f4bc24bbac3f20f1cb2835
toyorcee <oluwatoyosiolaniyan@gmail.com>
2025-02-01 05:59:21 +0100

    install mongoose mongodb

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -5,34 +5,51 @@ import { createOrUpdateUser, deleteUser } from "../../../lib/actions/user";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
   if (!SIGNING_SECRET) {
-    throw new Error("Missing SIGNING_SECRET in environment variables");
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local"
+    );
   }
 
+  // Create new Svix instance with secret
   const wh = new Webhook(SIGNING_SECRET);
-  const headerPayload = headers();
+
+  // Get headers
+  const headerPayload = await headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Missing Svix headers", { status: 400 });
+    return new Response("Error: Missing Svix headers", {
+      status: 400,
+    });
   }
 
+  // Get body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
   let evt;
+
+  // Verify payload with headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     });
-  } catch {
-    return new Response("Verification failed", { status: 400 });
+  } catch (err) {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
+      status: 400,
+    });
   }
 
+  // Do something with payload
+  // For this guide, log payload to console
   const { id } = evt?.data;
   const eventType = evt?.type;
 
@@ -51,22 +68,26 @@ export async function POST(req) {
           await clerkClient.updateUserMetadata(id, {
             publicMetadata: { userMongoId: user._id },
           });
-        } catch {
-          return new Response("Failed to update Clerk metadata", {
-            status: 400,
-          });
+        } catch (error) {
+          console.log("Error: Could not update user metadata:", error);
         }
       }
-    } catch {
-      return new Response("Failed to create/update user", { status: 400 });
+    } catch (error) {
+      console.log("Error: Could not create or update user:", error);
+      return new Response("Error: Could not create or update user", {
+        status: 400,
+      });
     }
   }
 
   if (eventType === "user.deleted") {
     try {
       await deleteUser(id);
-    } catch {
-      return new Response("Failed to delete user", { status: 400 });
+    } catch (error) {
+      console.log("Error: Could not delete user:", error);
+      return new Response("Error: Could not delete user", {
+        status: 400,
+      });
     }
   }
 
